fbca90f63f7c05f829b392e60ec5bf32ce438bfa
Niranjan <niranjanr753@gmail.com>
2024-09-06 00:22:23 +0530

    Configure for GitHub Pages deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/your-repo-name/',
+  basePath: '/your-repo-name',
+};
 
 export default nextConfig;
